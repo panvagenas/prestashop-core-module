434adcc75d9e78a87716b5ad9e278077ba9f891c
Panagiotis Vagenas <pan.vagenas@gmail.com>
2014-11-12 00:01:10 +0200

    Some core classes

--- core/Hooks.php
+++ core/Hooks.php
@@ -16,43 +16,6 @@ if ( ! defined( '_PS_VERSION_' ) ) {
 }
 
 class Hooks extends Singleton{
-	public static $displayHooks = array(
-		'displayFooter'
-	);
-
-	public static $actionHooks = array(
-		'actionAdminMetaControllerUpdate_optionsBefore',
-		'actionAdminMetaSave',
-		'actionAuthentication',
-		'actionBeforeAuthentication',
-		'actionCartSave',
-		'actionCustomerAccountAdd',
-		'actionHtaccessCreate',
-		'actionObjectCategoryDeleteAfter',
-		'actionObjectCategoryUpdateAfter',
-		'actionObjectCmsDeleteAfter',
-		'actionObjectCmsUpdateAfter',
-		'actionObjectManufacturerDeleteAfter',
-		'actionObjectManufacturerUpdateAfter',
-		'actionObjectProductDeleteAfter',
-		'actionObjectProductUpdateAfter',
-		'actionObjectSupplierDeleteAfter',
-		'actionObjectSupplierUpdateAfter',
-		'actionOrderDetail',
-		'actionOrderReturn',
-		'actionOrderSlipAdd',
-		'actionOrderStatusPostUpdate',
-		'actionOrderStatusUpdate',
-		'actionPaymentCCAdd',
-		'actionPaymentConfirmation',
-		'actionSearch',
-		'actionShopDataDuplication',
-		'actionTaxManager',
-		'actionUpdateQuantity',
-		'actionValidateOrder',
-		'actionWatermark',
-	);
-
 	public static function registerHooks( \Module $module, Hooks $class ) {
 		$hooks = (array) get_class_methods( get_class($class) );
 		$result = true;
