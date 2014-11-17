0655d2a881074614248e00e6ab5d3b4f825bc9fa
Panagiotis Vagenas <pan.vagenas@gmail.com>
2014-11-17 08:43:37 +0200

    Added Arrays methods, needs work here

---
+++ core/Exception.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Project: coremodule
+ * File: Exception.php
+ * User: Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * Date: 17/11/2014
+ * Time: 8:30 πμ
+ * Since: TODO ${VERSION}
+ * Copyright: 2014 Panagiotis Vagenas
+ */
+
+namespace XDaRk;
+
+if (!defined('_PS_VERSION_'))
+	exit;
+
+class Exception extends \Exception{
+
+} 
